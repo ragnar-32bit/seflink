9df5960d86190203b341fa4237e1033635e464d8
SHMidorima <64507597+shmidorima@users.noreply.github.com>
2020-06-01 01:40:29 +0300

    Add files via upload

---
+++ sef-link.php
@@ -0,0 +1,22 @@
+<?php 
+
+function seflink($str){
+    // karakterleri küçülttük
+    $str = mb_strtolower($str,'UTF-8');
+    // türkçe karakterleri dönüþtürdük
+    $str = str_replace(
+        ['ý','þ','ü','ö','ð','ç'],
+        ['i','s','u','o','g','c'],
+        $str
+        );
+    //boþluklara ve kelimelerin arasýna - koyacaðým
+    $str = preg_replace('/[^a-z0-9]/','-',$str);
+    // bir kere yazmak için
+    $str = preg_replace('/-+/','-',$str);
+    return trim($str,'-');
+}
+
+$str = 'This is a seflink / Kirathebug';
+echo seflink($str);
+
+?>
